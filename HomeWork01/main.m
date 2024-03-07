68ab666858964bf25e308afff5f587111644b81c
Michanco <michanco80@gmail.com>
2024-03-07 01:04:00 -0800

    HW1 06.03.24

--- HomeWork01/main.m
+++ HomeWork01/main.m
@@ -10,8 +10,27 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        
-        
+        NSLog(@" Enter coefficients a b c ");
+        double a, b, c, discr;
+        scanf("%lf", &a);
+        scanf("%lf", &b);
+        scanf("%lf", &c);
+        discr = b * b - 4 * a * c;
+        if (discr < 0) {
+            NSLog(@"The equation has no roots");
+        } else if (discr == 0){
+            NSLog(@"The equation has one root X = %f", -b/(2 * a));
+        } else {
+            NSLog(@"The equation has two roots X1 = %f X2 = %f", (-b + sqrt(discr)) / (2 * a), (-b - sqrt(discr)) / (2 * a));
+        }
+        double max = a;
+        if (b > max) {
+            max = b;
+        }
+        if (c > max) {
+            max = c;
+        }
+        NSLog(@"Maximum coefficient - %.2f", max);
     }
     return 0;
 }
