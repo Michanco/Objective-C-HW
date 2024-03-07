3eaa3071ff16e7c4a5a8db7370e4f88ff814755b
Michanco <michanco80@gmail.com>
2024-03-07 00:03:09 -0800

    Initial Commit

---
+++ HomeWork01/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HomeWork01
+//
+//  Created by Michanco Slesarev on 06.03.2024.
+//
+
+#import <Foundation/Foundation.h>
+#import <math.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        
+    }
+    return 0;
+}
